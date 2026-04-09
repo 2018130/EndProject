f2d81fd5f2932e93873905cf6e6cb9d2b31efdd1
donghuian289-cloud <donghuian289@gmail.com>
2026-04-09 17:50:07 +0900

    MapTest

--- Assets/3.Script/Manager/GameDataManager.cs
+++ Assets/3.Script/Manager/GameDataManager.cs
@@ -53,7 +53,7 @@ public class GameDataManager : NetworkBehaviour
             weaponNO.SpawnWithOwnership(clientId);
 
             if (NetworkManager.Singleton.ConnectedClients
-                .TryGetValue(clientId, out NetworkClient client))
+                .TryGetValue(clientId, out Unity.Netcode.NetworkClient client))
             {
 
                 weaponNO.TrySetParent(client.PlayerObject, false);
@@ -74,7 +74,7 @@ public class GameDataManager : NetworkBehaviour
             .TryGetValue(weaponNetworkId, out NetworkObject weaponNO)) return;
 
         if (!NetworkManager.Singleton.ConnectedClients
-            .TryGetValue(clientId, out NetworkClient client)) return;
+            .TryGetValue(clientId, out Unity.Netcode.NetworkClient client)) return;
 
         BaseWeapon weapon = weaponNO.GetComponent<BaseWeapon>();
         weapon.transform.localPosition = new Vector3(0.7f, 0.7f, 0f);
